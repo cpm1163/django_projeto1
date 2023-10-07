58c07d95bc7e160d347957a63e85d72754a7de3a
cpm1163 <carlos.mahet@gmail.com>
2023-10-07 19:37:26 -0300

    configuraçao do static

--- recipes/views.py
+++ recipes/views.py
@@ -4,4 +4,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Carlo M'
+    })
