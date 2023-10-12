59c3fe502df6d9588a3b279acebfdae2a6a90d26
cpm1163 <carlos.mahet@gmail.com>
2023-10-12 19:50:46 -0300

    Ate a aula 43

--- recipes/views.py
+++ recipes/views.py
@@ -7,3 +7,9 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Carlo M'
     })
+
+# Create your views here.
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'name': 'Carlo M'
+    })
