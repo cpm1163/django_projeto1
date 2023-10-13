163b86bbaf2938ed6d12ccd0f4711ae7b209244c
cpm1163 <carlos.mahet@gmail.com>
2023-10-13 20:39:16 -0300

    final aula 46

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from utils.recipes.factory import make_recipe
 
 # Create your views here.
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Carlo M'
+        'recipes': [make_recipe() for _ in range(10)],
     })
 
 # Create your views here.
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Carlo M'
+        'recipe': make_recipe(),
     })
