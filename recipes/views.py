2dbc935dd26577077dda8994b6e0b9583ffd8c15
cpm1163 <carlos.mahet@gmail.com>
2023-10-16 18:27:34 -0300

    Aula 58 Usando o model Recipe real (com QuerySet)

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from utils.recipes.factory import make_recipe
+from .models import Recipe
+
 
 # Create your views here.
 def home(request):
+    recipes = Recipe.objects.all().order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipes,
     })
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(
+        category__id=category_id
+        ).order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes,
+    })
+
 
 # Create your views here.
 def recipe(request, id):
